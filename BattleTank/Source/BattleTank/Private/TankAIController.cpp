66f3932f8e2f60bbb2bb78b1546e55ff281d4ea3
Boaz Yakubov <garret866@gmail.com>
2018-03-16 15:39:56 +0200

    after long hiatus, tansk finding player controller, end lecture 128

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,12 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
+#include "Engine/World.h"
 
 void ATankAIController::BeginPlay()
 {
 	//do some stuff at begin play
 	Super::BeginPlay();
 	auto possessedTank = GetControlledTank();
+	auto playerTank = GetPlayerTank();
 	if (possessedTank)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Tank Ai controller possesing %s"), *possessedTank->GetName());
@@ -15,6 +17,23 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Tank AI controller not possesing tank"));
 	}
+
+	if (playerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank Ai controller found player %s"), *playerTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank AI controller not finding player"));
+	}
+
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	ATank* PlayerTank = nullptr;
+	PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	return PlayerTank;
 }
 
 ATank* ATankAIController::GetControlledTank() const
