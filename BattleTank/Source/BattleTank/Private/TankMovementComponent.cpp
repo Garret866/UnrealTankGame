16dcae6add9ab7cdb7edb042721c289b70530eed
Boaz Yakubov <garret866@gmail.com>
2018-05-19 17:24:44 +0300

    start lectur 166 - i just put some turn right/foward backward func..

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,15 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+void UTankMovementComponent::Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+		LeftTrack = LeftTrackToSet;
+		RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend Move Forward throw: %f"),Throw);
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	//TODO prevent multiple controls setting throttle (forward + numpads) double speed
 }
 
-void UTankMovementComponent::IntendMoveBackward(float Throw)
+void UTankMovementComponent::IntendTurnRight(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend Move Backward throw: %f"), Throw);
-	//intendbro
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
+
+
+
+
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,12 +6,12 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
-class ATank;
+class UTankTrack;
 
 /**
  * 
  */
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent),ShowFunctions=(Custom))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -20,10 +20,12 @@ public:
 	UFUNCTION(BlueprintCallable,Category = Input)
 		void IntendMoveForward(float Throw);
 	UFUNCTION(BlueprintCallable, Category = Input)
-		void IntendMoveBackward(float Throw);
+		void IntendTurnRight(float Throw);
 
-protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-		ATank* Tank;
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 	
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 };
